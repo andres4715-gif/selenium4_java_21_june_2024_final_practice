f582bdd35e90eb7a933448a08859ea7da6180ea4
andres-rios-intelex <147648981+andres-rios-intelex@users.noreply.github.com>
2024-06-20 16:07:21 -0500

    new data added working with locators

---
+++ src/main/java/com/nopCommerce/locators/HomeNopCommerceLocators.java
@@ -0,0 +1,7 @@
+package com.nopCommerce.locators;
+
+import org.openqa.selenium.By;
+
+public class HomeNopCommerceLocators {
+    public static final By REGISTER_BUTTON = By.cssSelector(".ico-register");
+}
--- src/main/java/com/nopCommerce/pages/RegisterNopCommercePage.java
+++ src/main/java/com/nopCommerce/pages/RegisterNopCommercePage.java
@@ -1,28 +1,39 @@
 package com.nopCommerce.pages;
 
-import org.openqa.selenium.By;
+import com.nopCommerce.locators.RegisterNopCommerceLocators;
 import org.openqa.selenium.WebDriver;
 
 public class RegisterNopCommercePage extends BasePage {
-    private final By newsletter = By.id("Newsletter");
-
     public RegisterNopCommercePage(WebDriver driver) {
         super(driver);
     }
 
+    /**
+     * Checks the default status of the newsletter checkbox.
+     * This method verifies whether the newsletter checkbox is selected by default
+     * and prints the result to the console.
+     */
     public void checkNewsLetterDefaultStatus() {
-        if(driver.findElement(newsletter).isSelected()) {
+        if(driver.findElement(RegisterNopCommerceLocators.NEW_LETTER).isSelected()) {
             System.out.println("--- The news letter checkbox is selected by default.");
         } else {
             System.out.println("--- The news letter checkbox is not selected.");
         }
     }
 
+    /**
+     * Unchecks the newsletter checkbox if it is selected by default.
+     * This method first checks the default status of the newsletter checkbox,
+     * then unchecks it if it is selected. Finally, it returns the current
+     * selected status of the checkbox.
+     *
+     * @return {@code false} if the newsletter checkbox is unchecked, {@code true} otherwise
+     */
     public boolean uncheckNewsLetterCheckbox() {
         checkNewsLetterDefaultStatus();
-        if(driver.findElement(newsletter).isSelected()) {
-            driver.findElement(newsletter).click();
+        if(driver.findElement(RegisterNopCommerceLocators.NEW_LETTER).isSelected()) {
+            driver.findElement(RegisterNopCommerceLocators.NEW_LETTER).click();
         }
-        return driver.findElement(newsletter).isSelected();
+        return driver.findElement(RegisterNopCommerceLocators.NEW_LETTER).isSelected();
     }
 }
