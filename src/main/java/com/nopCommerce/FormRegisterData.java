5f5f866113cf35646ee5c910982dbaf07b314a6e
andres-rios-intelex <147648981+andres-rios-intelex@users.noreply.github.com>
2024-06-27 08:17:16 -0500

    lombok added working fine

--- src/main/java/com/nopCommerce/FormRegisterData.java
+++ src/main/java/com/nopCommerce/FormRegisterData.java
@@ -1,5 +1,15 @@
 package com.nopCommerce;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+/**
+ * This class represents the data for registering a user.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FormRegisterData {
 
     private String firstName;
@@ -8,63 +18,5 @@ public class FormRegisterData {
     private String companyName;
     private String password;
     private String confirmPassword;
-
     private String day;
-
-    // Getters and Setters
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
-    }
-
-    public String getDay() {
-        return day;
-    }
-
-    public void setDay(String day) {
-        this.day = day;
-    }
 }
