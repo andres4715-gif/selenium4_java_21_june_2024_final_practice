fff1a3c0c85f1c810a1388a28df1208f922b68ab
andres-rios-intelex <147648981+andres-rios-intelex@users.noreply.github.com>
2024-06-29 11:41:22 -0500

    new data added

--- src/test/java/com/nopCommerce/utils/Hooks.java
+++ src/test/java/com/nopCommerce/utils/Hooks.java
@@ -26,7 +26,7 @@ public class Hooks {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
 
-        // GITHUB Action execution "Check tap action on the Github repository"
+        // GITHUB Action execution
         String seleniumRemoteUrl = System.getenv("SELENIUM_REMOTE_URL");
         if (seleniumRemoteUrl != null && !seleniumRemoteUrl.isEmpty()) {
             try {
