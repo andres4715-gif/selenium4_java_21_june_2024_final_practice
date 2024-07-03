640d4f9e176bee38d4e67f28c0470e94c3933b08
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2024-07-03 08:03:50 -0500

    managing the driver (#25)
    Co-authored-by: andres-rios-intelex <147648981+andres-rios-intelex@users.noreply.github.com>

---
+++ src/test/java/com/nopCommerce/steps/HomeSteps.java
@@ -0,0 +1,31 @@
+package com.nopCommerce.steps;
+
+import com.nopCommerce.pages.HomeNopCommercePage;
+import com.nopCommerce.utils.Hooks;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+
+public class HomeSteps {
+    private static final Logger logger = LoggerFactory.getLogger(HomeSteps.class);
+    private WebDriver driver = Hooks.getDriver();
+    HomeNopCommercePage homeNopCommercePage;
+
+    @Given("the user is on the nonCommerce page")
+    public void the_user_is_on_the_nonCommerce_page() {
+        logger.info("--- Navigating to nonCommerce page");
+        driver = Hooks.getDriver();
+        driver.get(Hooks.getBaseUrl());
+    }
+
+    @When("the user clicks on register")
+    public void the_user_clicks_on_register() {
+        homeNopCommercePage = new HomeNopCommercePage(driver);
+        logger.info("--- The obtained Title on Home page is: " + homeNopCommercePage.gitTitle());
+        Assert.assertEquals(driver.getTitle(), "nopCommerce demo store");
+        homeNopCommercePage.clickOnRegisterButton();
+    }
+}
