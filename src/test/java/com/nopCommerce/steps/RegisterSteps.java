c55b5b35e01cb53170e64d75c61d96489f75a40c
Andres Rios <andres.rios@intelex.com>
2025-01-29 07:55:07 -0500

    Adding baseStep class

--- src/test/java/com/nopCommerce/steps/RegisterSteps.java
+++ src/test/java/com/nopCommerce/steps/RegisterSteps.java
@@ -1,31 +1,25 @@
 package com.nopCommerce.steps;
 
-import com.nopCommerce.models.FormRegisterData;
 import com.nopCommerce.helpers.FormRegisterDataHelper;
-import com.nopCommerce.pages.RegisterNopCommercePage;
-import com.nopCommerce.utils.Hooks;
+import com.nopCommerce.models.FormRegisterData;
 import io.cucumber.java.en.Then;
-import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 
-public class RegisterSteps {
+public class RegisterSteps extends BaseSteps {
     private static final Logger logger = LoggerFactory.getLogger(RegisterSteps.class);
-    private WebDriver driver = Hooks.getDriver();
-
-    RegisterNopCommercePage registerNopCommercePage = new RegisterNopCommercePage(driver);
 
     @Then("the user checks the checkbox functionality working properly")
     public void the_user_checks_the_checkbox_functionality_working_properly() {
-        logger.info("--- The obtained Title on Register page is: " + registerNopCommercePage.getTitle());
-        Assert.assertEquals(driver.getTitle(), "nopCommerce demo store. Register");
+        logger.info("--- The obtained Title on Register page is: {}", registerNopCommercePage.getTitle());
+        Assert.assertEquals(driver.getTitle(), "nopCommerce demo store. Register"); // arreglar esto
         Assert.assertFalse(registerNopCommercePage.uncheckNewsLetterCheckbox());
     }
 
     @Then("the user adds the personal information, on the Register Page")
     public void the_user_adds_the_personal_information_on_the_Register_Page(io.cucumber.datatable.DataTable dataTable) {
-        logger.info("--- ✅✅✅ Filling data from dataTable");
+        logger.info("--- Filling data from dataTable");
         FormRegisterData formRegisterData = FormRegisterDataHelper.convertDataTableToFormRegisterData(dataTable);
         registerNopCommercePage.fillRegisterForm(formRegisterData);
     }
